cfa93b6954e2e18771bbccc3d43d35b3c5b7f433
Rene Steinbach <r.steinbach@henova.de>
2018-09-12 17:12:29 +0200

    einige Änderung

--- OmbiReleaseFinder/OmbiReleaseFinder/wwwroot/js/site.js
+++ OmbiReleaseFinder/OmbiReleaseFinder/wwwroot/js/site.js
@@ -27,3 +27,33 @@
     //    });
     //});
 });
+
+$(function () {
+    var placeholderElement = $('#modal-placeholder');
+
+    $('input[data-toggle="ajax-modal"]').click(function (event) {
+        var url = $(this).data('url');
+        var search = $(this).data('search');
+        var themoviedbid = $(this).data('movieid');
+        var newname = url + search + themoviedbid;
+        $.get(newname).done(function (data) {
+            placeholderElement.html(data);
+            placeholderElement.find('.modal').modal('show');
+        });
+    });
+
+    //placeholderElement.on('click', '[data-save="modal"]', function (event) {
+    //    event.preventDefault();
+
+    //    var form = $(this).parents('.modal').find('form');
+    //    var actionUrl = form.attr('action');
+    //    var dataToSend = form.serialize();
+
+    //    $.post(actionUrl, dataToSend).done(function (data) {
+    //        var newBody = $('.modal-body', data);
+    //        placeholderElement.find('.modal-body').replaceWith(newBody);
+
+
+    //    });
+    //});
+});
